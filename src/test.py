cb37ce9b63746ddd6daea7d050fe58e014c88890
Jon Craton <jncraton@gmail.com>
2024-11-14 15:41:54 -0500

    Modernize test

--- src/test.py
+++ src/test.py
@@ -1,32 +1,35 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
 
 import os
 import difflib
 
-options = Options()
-options.headless = True
+options = webdriver.FirefoxOptions()
+options.add_argument("--headless")
 
 with webdriver.Firefox(options=options) as browser:
     browser.get(f'file://{os.getcwd()}/dist/index.html')
 
     def assert_contents():
         expected = '<section style="width: 126px; height: 190px; left: 62px; top: 62px;"><p contenteditable="true">Box</p></section><section style="width: 126px; left: 190px; top: 190px;"><p style="top: -66px;" contenteditable="true">Line</p></section><p style="left: 318px; top: 190px;" contenteditable="true">Text♥</p><section style="width: 126px; height: 126px; left: 510px; top: 190px;"><p contenteditable="true">Small Box</p></section><section style="width: 126px; left: 638px; top: 382px;" data-decoration="1"><p style="top: -66px;" contenteditable="true">Right</p><arrow class="right"></arrow></section><section style="width: 126px; height: 126px; left: 958px; top: 382px;" data-decoration="3"><p contenteditable="true">Has color</p></section>'
-        content = browser.find_element_by_css_selector('body').get_attribute('innerHTML')
+        content = browser.find_element(By.CSS_SELECTOR, 'body').get_attribute('innerHTML')
         content = content.strip().replace('<br>', '')
+        content = content.replace(' contenteditable="true"', '')
+        expected = expected.replace(' contenteditable="true"', '')
         if expected != content:
             for diff in difflib.ndiff(expected.replace('<', '\n<').splitlines(), content.replace('<', '\n<').splitlines()):
                 print(diff)
             raise AssertionError
 
     def create_contents():
-        body = browser.find_element_by_css_selector('body')
+        action = ActionBuilder(browser)
+        action.pointer_action.move_to_location(0, 0)
+        action.perform()
 
         # Create a box
         webdriver.ActionChains(browser)\
-            .move_to_element_with_offset(body, 0, 0)\
             .move_by_offset(50, 50)\
             .click_and_hold()\
             .move_by_offset(150, 200)\
@@ -97,13 +100,13 @@ with webdriver.Firefox(options=options) as browser:
     # Delete all elements via Shift+click
     # We may need to run this multiple time to remove elements hidden behind others
     for _ in range(2):
-        for e in browser.find_elements_by_css_selector('body>*'):
+        for e in browser.find_elements(By.CSS_SELECTOR, 'body>*'):
             webdriver.ActionChains(browser)\
                 .key_down(Keys.SHIFT)\
                 .click(e)\
                 .key_up(Keys.SHIFT)\
                 .perform()
-    assert(browser.find_element_by_css_selector('body').get_attribute('innerHTML').strip() == '')
+    assert(browser.find_element(By.CSS_SELECTOR, 'body').get_attribute('innerHTML').strip() == '')
 
     create_contents()
 
